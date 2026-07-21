58fb21c353c674e0001be07314e450cac01f5f98
Daniel <daniburgas2003@gmail.com>
2026-07-21 16:34:15 +0300

    Add movie service impl

---
+++ src/main/java/com/example/movielibrary/services/MovieService.java
@@ -0,0 +1,19 @@
+package com.example.movielibrary.services;
+
+import com.example.movielibrary.models.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+
+    List<Movie> getAll();
+
+    Movie getById(int id);
+
+    Movie getByTitle(String title);
+
+    void create(Movie movie);
+    void update(int id, Movie movie);
+    void delete(int id);
+
+}
---
+++ src/main/java/com/example/movielibrary/services/MovieServiceImpl.java
@@ -0,0 +1,65 @@
+package com.example.movielibrary.services;
+
+import com.example.movielibrary.exceptions.EntityNotFoundException;
+import com.example.movielibrary.models.Movie;
+import com.example.movielibrary.repositories.MovieRepository;
+import jakarta.persistence.NoResultException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+    MovieRepository repository;
+
+    @Autowired
+    public MovieServiceImpl(MovieRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public Movie getById(int id) {
+        try{
+            return repository.getById(id);
+        }catch (NoResultException e){
+            throw new EntityNotFoundException("Not Found!"); //TODO fix
+        }
+    }
+
+    @Override
+    public Movie getByTitle(String title) {
+        try{
+            return repository.getByTitle(title);
+        }catch (NoResultException e){
+            throw new EntityNotFoundException("Not Found!"); //TODO fix
+        }
+    }
+
+    @Override
+    public void create(Movie movie) {
+
+        repository.create(movie);
+    }
+
+    @Override
+    public void update(int id, Movie movie) {
+
+        Movie movieToUpdate = getById(id);
+
+        repository.update(movieToUpdate);
+    }
+
+    @Override
+    public void delete(int id) {
+
+        Movie movie = getById(id);
+        repository.delete(movie);
+    }
+}
